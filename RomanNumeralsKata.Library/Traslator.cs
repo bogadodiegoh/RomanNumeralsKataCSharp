f8065bc9078e04fb0fbd79b13ef8bbfdb3dcee55
Diego Bogado <diegobogado@live.com.ar>
2013-04-05 08:40:23 -0300

    1er commit
    -solo convierte a numeros romanos.
    TODO: convertir desde romanos a arabigos

---
+++ RomanNumeralsKata.Library/Traslator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumeralsKata.Library
+{
+    public class Traslator
+    {
+        private string Result { get; set; }
+
+        private readonly IDictionary<string, string> _oneSymbolDictionary;
+        private readonly IList<string> _units;
+        private readonly IList<string> _tens;
+        private readonly IList<string> _hundreds;
+
+        public Traslator()
+        {
+            _oneSymbolDictionary = new Dictionary<string, string>
+                                    {{"1","I"},{"5","V"},
+                                    {"10","X"},{"50","L"},
+                                    {"100","C"},{"500","D"},
+                                    {"1000","M"}};
+            _units = new List<string> { "V", "IV", "IX", "I" };
+            _tens = new List<string> { "L", "XL", "XC", "X" };
+            _hundreds = new List<string> { "D", "CD", "CM", "C" };
+        }
+
+        public string ConvertToRomanNumber(string value)
+        {            
+            Result = string.Empty;
+            if (IsOnlyOneLetter(value))
+                GetOneLetter(value);
+            else
+            {
+                if(value.Length>2)GetCorrectSymbols(value.Substring(value.Length - 3,1),_hundreds);
+                if (value.Length > 1)GetCorrectSymbols(value.Substring(value.Length - 2, 1), _tens);
+                if (value.Length > 0)GetCorrectSymbols(value.Substring(value.Length - 1), _units);
+            }
+            return Result;
+        }
+
+        private void GetCorrectSymbols(string value,IList<string> symbols)
+        {           
+            switch (value)
+            {
+                case "5":
+                    Result += symbols[0];
+                    break;
+                case "4":
+                    Result += symbols[1];
+                    break;
+                case "9":
+                    Result += symbols[2];
+                    break;
+                default:
+                    var num = Convert.ToInt32(value);
+                    if (num > 5)
+                    {
+                        Result += symbols[0];
+                        num -= 5;
+                    }                    
+                    AddFromOneToThreeToEnd(num,symbols[3]);
+                    break;
+            }            
+        }
+
+        private void AddFromOneToThreeToEnd(int number, string unit)
+        {
+            for (var i = 1; i <= number; i++)
+                Result += unit;
+        }  
+ 
+        private void GetOneLetter(string number)
+        {                      
+            Result = _oneSymbolDictionary[number];                       
+        }
+
+        private bool IsOnlyOneLetter(string number)
+        {
+           return _oneSymbolDictionary.ContainsKey(number);
+        }         
+    }
+}
---
+++ RomanNumeralsKata.Test/TraslatorTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RomanNumeralsKata.Library;
+
+namespace RomanNumeralsKata.Test
+{
+    [TestFixture]
+    public class TraslatorTest
+    {
+        private Traslator _traslator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _traslator = new Traslator();
+        }
+
+        [Test]
+        public void ShouldConvertSingleNumbers()
+        {            
+            Assert.AreEqual("I", _traslator.ConvertToRomanNumber("1"));
+            Assert.AreEqual("V", _traslator.ConvertToRomanNumber("5"));
+            Assert.AreEqual("X", _traslator.ConvertToRomanNumber("10"));
+            Assert.AreEqual("L", _traslator.ConvertToRomanNumber("50"));
+            Assert.AreEqual("C", _traslator.ConvertToRomanNumber("100"));
+            Assert.AreEqual("D", _traslator.ConvertToRomanNumber("500"));
+            Assert.AreEqual("M", _traslator.ConvertToRomanNumber("1000"));
+        }
+
+        [Test]
+        public void ShouldConvertFromTwoToFive()
+        {
+            Assert.AreEqual("II", _traslator.ConvertToRomanNumber("2"));
+            Assert.AreEqual("III", _traslator.ConvertToRomanNumber("3"));
+            Assert.AreEqual("IV", _traslator.ConvertToRomanNumber("4"));
+            Assert.AreEqual("V", _traslator.ConvertToRomanNumber("5"));          
+        }
+
+        [Test]
+        public void ShouldConvertFromSixToTen()
+        {
+            Assert.AreEqual("IX", _traslator.ConvertToRomanNumber("9"));
+            Assert.AreEqual("VIII", _traslator.ConvertToRomanNumber("8"));
+            Assert.AreEqual("VII", _traslator.ConvertToRomanNumber("7"));
+            Assert.AreEqual("VI", _traslator.ConvertToRomanNumber("6"));
+        }
+
+        [Test]
+        public void ShouldConvertFromTenToFifty()
+        {
+            Assert.AreEqual("XV", _traslator.ConvertToRomanNumber("15"));
+            Assert.AreEqual("XXV", _traslator.ConvertToRomanNumber("25"));
+            Assert.AreEqual("XL", _traslator.ConvertToRomanNumber("40"));            
+        }
+
+        [Test]
+        public void ShouldConvertFromFiftyToOneHundred()
+        {
+            Assert.AreEqual("LV", _traslator.ConvertToRomanNumber("55"));
+            Assert.AreEqual("LXXVIII", _traslator.ConvertToRomanNumber("78"));
+            Assert.AreEqual("XC", _traslator.ConvertToRomanNumber("90"));
+        }
+
+        [Test]
+        public void ShouldConvertFromOneHundredToFiveHundred()
+        {
+            Assert.AreEqual("CCLV", _traslator.ConvertToRomanNumber("255"));
+            Assert.AreEqual("CCCLXXVIII", _traslator.ConvertToRomanNumber("378"));
+            Assert.AreEqual("D", _traslator.ConvertToRomanNumber("500"));
+        }
+
+        [Test]
+        public void ShouldConvertFromFiveHundredToOneThousands()
+        {
+            Assert.AreEqual("DXVII", _traslator.ConvertToRomanNumber("517"));
+            Assert.AreEqual("DCCCXXXIX", _traslator.ConvertToRomanNumber("839"));
+            Assert.AreEqual("M", _traslator.ConvertToRomanNumber("1000"));
+        }
+    }
+}
